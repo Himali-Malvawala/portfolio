73d3528eff2b685a18b5bf44df525eb52b7bd73b
Himali Malvawala <himalimalvawala@gmail.com>
2026-09-15 17:04:10 +0530

    add /hobbies page

---
+++ app/hobbies/page.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from "../components/PageLayout";
+import { WorkInProgress } from "../components/WorkInProgress";
+
+export default function Hobbies() {
+  return (
+    <PageLayout pageTitle="Hobbies" active="hobbies">
+      <WorkInProgress />
+    </PageLayout>
+  );
+}
